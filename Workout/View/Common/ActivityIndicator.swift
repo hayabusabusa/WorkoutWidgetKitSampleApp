1ab0a43568e4b1c4ba3060f765f5b7d4ce3372c9
hayabusabusa <hayabusabusaSY@gmail.com>
2020-12-15 19:07:59 +0900

    Add activity indicator view wrapped UIViewRepresentable

---
+++ Workout/View/Common/ActivityIndicator.swift
@@ -0,0 +1,24 @@
+//
+//  ActivityIndicator.swift
+//  Workout
+//
+//  Created by 山田隼也 on 2020/12/15.
+//
+
+import SwiftUI
+import UIKit
+
+struct ActivityIndicator: UIViewRepresentable {
+    typealias UIViewType = UIActivityIndicatorView
+    
+    @Binding var isAnimating: Bool
+    let style: UIActivityIndicatorView.Style = .medium
+    
+    func makeUIView(context: Context) -> UIActivityIndicatorView {
+        return UIActivityIndicatorView(style: style)
+    }
+    
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: Context) {
+        isAnimating ? uiView.startAnimating() : uiView.stopAnimating()
+    }
+}
